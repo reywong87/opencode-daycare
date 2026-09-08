f8b0baf9af8cdf96ddc9d0f654311a34799d477f
Rey <rey.wong87@gmail.com>
2026-09-08 15:32:22 +0200

    Add authorization and data protection services to Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddDataProtection();
 
 var supabaseUrl = builder.Configuration["Supabase:Url"]
                   ?? throw new InvalidOperationException("Missing Supabase:Url.");
