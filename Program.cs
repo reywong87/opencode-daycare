05551e85a2629fdc74b48ab81798cba8e5dfa392
Rey <rey.wong87@gmail.com>
2026-09-08 19:37:50 +0200

    Add SupabaseAuthenticationStateProvider to manage authentication state and user claims

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using OpenDaycare.Components;
 using OpenDaycare.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddDataProtection();
 builder.Services.AddScoped<SupabaseAuthService>();
+builder.Services.AddScoped<SupabaseAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(serviceProvider => serviceProvider.GetRequiredService<SupabaseAuthenticationStateProvider>());
 
 var supabaseUrl = builder.Configuration["Supabase:Url"]
                   ?? throw new InvalidOperationException("Missing Supabase:Url.");
